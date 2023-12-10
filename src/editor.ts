24c86c2d4038f27f6ee834bfa398181d68a22a90
3-masato <lzxqt0@gmail.com>
2023-12-10 14:38:41 +0900

    Update: Pixelクラスのコメントを追加

--- src/editor.ts
+++ src/editor.ts
@@ -10,27 +10,61 @@ export type PixelArtEventMap = {
   clear: void;
 };
 
+/**
+ * ピクセルバッファを表すクラス
+ */
 class Pixel {
+  /** ピクセルバッファの幅 */
   width: number;
+
+  /** ピクセルバッファの高さ */
   height: number;
+
+  /** ピクセルデータを格納するバッファ */
   buffer: Uint32Array;
 
+  /**
+   * Pixel インスタンスを作成します。
+   *
+   * @param {number} width - ピクセルバッファの幅
+   * @param {number} height - ピクセルバッファの高さ
+   */
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
     this.buffer = new Uint32Array(width * height);
   }
 
+  /**
+   * 指定された座標のピクセル色を設定します。
+   *
+   * @param {number} x - ピクセルのx座標
+   * @param {number} y - ピクセルのy座標
+   * @param {number} color - 指定座標に設定する色
+   */
   set(x: number, y: number, color: number): void {
     if (!this.contain(x, y)) return;
     this.buffer[y * this.width + x] = color;
   }
 
+  /**
+   * 指定された座標のピクセル色を取得します。
+   *
+   * @param {number} x - ピクセルのx座標
+   * @param {number} y - ピクセルのy座標
+   * @returns {number | undefined} 指定された座標の色、または範囲外の場合は `undefined`
+   */
   get(x: number, y: number): number | undefined {
     if (!this.contain(x, y)) return undefined;
     return this.buffer[y * this.width + x];
   }
 
+  /**
+   * 指定された座標がバッファの範囲内にあるかどうかを確認します。
+   * @param {number} x - 確認するx座標
+   * @param {number} y - 確認するy座標
+   * @returns {boolean} 座標が範囲内の場合は `true`、そうでない場合は `false`
+   */
   contain(x: number, y: number): boolean {
     return x >= 0 && y >= 0 && x < this.width && y < this.height;
   }
